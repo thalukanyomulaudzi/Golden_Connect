b7f20cd9da3c9aea29c3f84df2b6b6df1727159f
Nhlamulo Maluleka <u15231748@tuks.co.za>
2019-08-06 11:59:36 +0200

    Added a global mysql connection class

---
+++ Design370/MysqlConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Design370
+{
+    class MysqlConnection
+    {
+        public static string conn = "datasource=127.0.0.1;username=root;password=;database=golden_connect";
+        MySqlConnection mysqlCon = new MySqlConnection(conn);
+        MySqlCommand cmd;
+        MySqlDataReader reader;
+        MySqlDataAdapter adapter;
+    }
+}
