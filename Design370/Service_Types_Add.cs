35da4dfa49a45e5302e6709efc6ca250827e8696
thalukanyomulaudzi <thalu.tuks@gmail.com>
2019-08-11 20:35:40 +0200

    Service and service type

--- Design370/Service_Types_Add.cs
+++ Design370/Service_Types_Add.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Design370
 {
@@ -20,6 +21,31 @@ namespace Design370
         private void Button2_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ServiceTypeInsert = "INSERT INTO service_type(service_type_name, service_type_description) VALUES('" +
+                                    txtServTypeName.Text + "', '" + txtServTypeDesc.Text + "')";
+            MysqlConnection.cmd = new MySqlCommand(ServiceTypeInsert, MysqlConnection.mysqlCon);
+            try
+            {
+                MysqlConnection.mysqlCon.Open();
+                MySqlDataReader checkIfExist = MysqlConnection.cmd.ExecuteReader();
+                if (checkIfExist.HasRows)
+                {
+                    MessageBox.Show("Service Type already exits!");
+                }
+                else
+                {
+                    MessageBox.Show("New Service Type Inserted!");
+                    MysqlConnection.mysqlCon.Close();
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("Error: " + ee.Message);
+            }
         }
     }
 }
