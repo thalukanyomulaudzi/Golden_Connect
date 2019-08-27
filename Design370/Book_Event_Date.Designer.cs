2c9ddd4a306acee75bbfdad6d0d44274686a9113
Gordon Fogwill <gordon.fogwill.gf@gmail.com>
2019-08-27 08:25:03 +0200

    booking v2

--- Design370/Book_Event_Date.Designer.cs
+++ Design370/Book_Event_Date.Designer.cs
@@ -152,7 +152,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(972, 554);
+            this.ClientSize = new System.Drawing.Size(1069, 554);
             this.Controls.Add(this.dgvBookEvent);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dateTimePicker2);
