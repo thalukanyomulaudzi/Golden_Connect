fe0e7eb25363fac6b0f1418684cf004be09dd007
Gordon Fogwill <gordon.fogwill.gf@gmail.com>
2019-08-28 12:58:22 +0200

    booking

--- Design370/Bookings.cs
+++ Design370/Bookings.cs
@@ -16,26 +16,40 @@ namespace Design370
                 DBConnection dBCon = DBConnection.Instance();
                 if (dBCon.IsConnect())
                 {
-                    string query = "SELECT booking_id FROM booking WHERE";
-
-
-
-
-
-
-                    query = "SELECT * FROM booking b JOIN customer c ON b.customer_id = c.customer_id WHERE c.customer_last LIKE %" + search + "%";
+                    List<short> bookingIDs = new List<short>();
+                    List<DateTime> bookingDates = new List<DateTime>();
+                    string query = "SELECT DISTINCT et.booking_id, t.timeslot_date FROM employee_timeslot et JOIN timeslot t ON et.timeslot_id = t.timeslot_id WHERE et.booking_id IS NOT NULL";
+                    System.Windows.Forms.MessageBox.Show(query);
                     var command = new MySqlCommand(query, dBCon.Connection);
                     var reader = command.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        bookingIDs.Add(reader.GetInt16(0));
+                        bookingDates.Add(DateTime.Parse(reader.GetString(1)));
+                        System.Windows.Forms.MessageBox.Show("Test");
+                    }
+                    reader.Close();
+
+                    query = search != "" ? "SELECT * FROM booking b JOIN customer c ON b.customer_id = c.customer_id WHERE c.customer_last LIKE %" + search + "%"
+                        : "SELECT * FROM booking b JOIN customer c ON b.customer_id = c.customer_id";
+                    command = new MySqlCommand(query, dBCon.Connection);
+                    reader = command.ExecuteReader();
                     while (reader.Read())
                     {
 
                     }
+                    reader.Close();
+
                 }
             }
             catch(Exception e)
             {
                 System.Windows.Forms.MessageBox.Show(e.Message);
             }
+        }
+        public static void loadEmployeeAvailable()
+        {
+
         }
     }
 }
