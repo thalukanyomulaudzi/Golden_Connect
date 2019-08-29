042e9da3a7567fb5ef1c8221797cabf6775db7e6
Gordon Fogwill <gordon.fogwill.gf@gmail.com>
2019-08-29 12:10:04 +0200

    booking renames

--- Design370/Booking_Details.Designer.cs
+++ Design370/Booking_Details.Designer.cs
@@ -50,7 +50,7 @@
             this.label9 = new System.Windows.Forms.Label();
             this.dtmBookingDate = new System.Windows.Forms.DateTimePicker();
             this.btnBookingChangeCustomer = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnBookingChangeDateEmployee = new System.Windows.Forms.Button();
             this.label10 = new System.Windows.Forms.Label();
             this.txtBookingEmployee = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numBookingGuests)).BeginInit();
@@ -186,6 +186,7 @@
             this.cmbBookingPackage.Name = "cmbBookingPackage";
             this.cmbBookingPackage.Size = new System.Drawing.Size(259, 26);
             this.cmbBookingPackage.TabIndex = 26;
+            this.cmbBookingPackage.SelectedIndexChanged += new System.EventHandler(this.CmbBookingPackage_SelectedIndexChanged);
             // 
             // label3
             // 
@@ -278,14 +279,15 @@
             this.btnBookingChangeCustomer.UseVisualStyleBackColor = true;
             this.btnBookingChangeCustomer.Click += new System.EventHandler(this.BtnBookingChangeCustomer_Click);
             // 
-            // button1
+            // btnBookingChangeDateEmployee
             // 
-            this.button1.Location = new System.Drawing.Point(464, 54);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(68, 60);
-            this.button1.TabIndex = 44;
-            this.button1.Text = "Change";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnBookingChangeDateEmployee.Location = new System.Drawing.Point(464, 54);
+            this.btnBookingChangeDateEmployee.Name = "btnBookingChangeDateEmployee";
+            this.btnBookingChangeDateEmployee.Size = new System.Drawing.Size(68, 60);
+            this.btnBookingChangeDateEmployee.TabIndex = 44;
+            this.btnBookingChangeDateEmployee.Text = "Change";
+            this.btnBookingChangeDateEmployee.UseVisualStyleBackColor = true;
+            this.btnBookingChangeDateEmployee.Click += new System.EventHandler(this.BtnBookingChangeDateEmployee_Click);
             // 
             // label10
             // 
@@ -312,7 +314,7 @@
             this.ClientSize = new System.Drawing.Size(544, 795);
             this.Controls.Add(this.txtBookingEmployee);
             this.Controls.Add(this.label10);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnBookingChangeDateEmployee);
             this.Controls.Add(this.btnBookingChangeCustomer);
             this.Controls.Add(this.dtmBookingDate);
             this.Controls.Add(this.label9);
@@ -371,7 +373,7 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.DateTimePicker dtmBookingDate;
         private System.Windows.Forms.Button btnBookingChangeCustomer;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnBookingChangeDateEmployee;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.TextBox txtBookingEmployee;
     }
