a6d1d50f65855d0d752708114dc259eee7a2bcdb
Stefan <Stefan@DESKTOP-0JL5FE4>
2019-09-25 11:39:44 +0200

    Customer Add

--- Design370/Customer_Add.cs
+++ Design370/Customer_Add.cs
@@ -16,6 +16,13 @@ namespace Design370
         public Customer_Add()
         {
             InitializeComponent();
+            ToolTip toolTip1 = new ToolTip();
+            toolTip1.ShowAlways = true;
+            toolTip1.SetToolTip(txtFN, "A maximum of 25 characters can be entered");
+            toolTip1.SetToolTip(txtLN, "A maximum of 25 characters can be entered");
+            toolTip1.SetToolTip(txtEM, "A maximum of 50 characters can be entered");
+            toolTip1.SetToolTip(txtTEL, "A maximum of 10 characters can be entered");
+            toolTip1.SetToolTip(textBox3, "A maximum of 13 characters can be entered");
         }
 
         private void Customer_Add_Load(object sender, EventArgs e)
@@ -25,35 +32,63 @@ namespace Design370
 
         private void Button1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
 
         private void Button2_Click_1(object sender, EventArgs e)
         {
             this.Close();
-            //It is working................
-            //Try This
-            //Again
         }
 
         private void Button1_Click_1(object sender, EventArgs e)
         {
-            //It's workingyyyy
-            //Stefan
-            addCustomer();
+            if (txtFN.Text.Length <= 0)
+            {
+                MessageBox.Show("Please provide a first name");
+                return;
+            }
+            else if (txtLN.Text.Length <= 0)
+            {
+                MessageBox.Show("Please provide a last name");
+                return;
+            }
+            else if (textBox3.Text.Length <= 12)
+            {
+                MessageBox.Show("Please provide a valid id number");
+                return;
+            }
+            else if (txtTEL.Text.Length <= 9)
+            {
+                MessageBox.Show("Please provide a valid phone number");
+                return;
+            }
+            else if (txtEM.Text.Length <= 8)
+            {
+                MessageBox.Show("Please provide a valid email address");
+                return;
+            }
+            DBConnection dBConnection = DBConnection.Instance();
+            try
+            {
+                if (dBConnection.IsConnect())
+                {
+                    string query = "INSERT INTO customer(customer_first, customer_last, customer_email, customer_phone, customer_id_number) VALUES('" + txtFN.Text + "', " +
+                        "'" + txtLN.Text + "', '" + txtEM.Text.ToLower() + "', '" + txtTEL.Text + "', '" + textBox3.Text + "')";
+                    var command = new MySqlCommand(query, dBConnection.Connection);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+            this.Close();
         }
 
         private void Customer_Add_FormClosing(object sender, FormClosingEventArgs e)
         {
-            DialogResult exit = MessageBox.Show("Do you want to save these changes?", "Save changes", MessageBoxButtons.YesNo);
-            e.Cancel = exit == DialogResult.Yes ? false : true;
-            MessageBox.Show("New Customer Added Successfully!");
-;        }
 
-        public void addCustomer()
-        {
-            
         }
     }
 }
