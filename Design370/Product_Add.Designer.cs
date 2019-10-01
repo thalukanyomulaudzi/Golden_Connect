10dc11c24fd7d901046ccf70cbcc3c1f051ed298
thalukanyomulaudzi <thalu.tuks@gmail.com>
2019-10-01 09:38:19 +0200

    Received Supplier Orders

--- Design370/Product_Add.Designer.cs
+++ Design370/Product_Add.Designer.cs
@@ -44,7 +44,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Bahnschrift Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(247, 298);
+            this.button2.Location = new System.Drawing.Point(240, 257);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(87, 28);
@@ -80,7 +80,7 @@
             this.label4.Location = new System.Drawing.Point(30, 186);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.Size = new System.Drawing.Size(72, 13);
             this.label4.TabIndex = 72;
             this.label4.Text = "Product Type:";
             // 
@@ -104,7 +104,7 @@
             this.label3.Location = new System.Drawing.Point(267, 20);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.Size = new System.Drawing.Size(78, 13);
             this.label3.TabIndex = 70;
             this.label3.Text = "Product Price :";
             // 
@@ -126,7 +126,7 @@
             this.label2.Location = new System.Drawing.Point(30, 72);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.Size = new System.Drawing.Size(109, 13);
             this.label2.TabIndex = 68;
             this.label2.Text = "Product Description :";
             // 
@@ -147,14 +147,14 @@
             this.label1.Location = new System.Drawing.Point(30, 20);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.Size = new System.Drawing.Size(81, 13);
             this.label1.TabIndex = 66;
             this.label1.Text = "Product Name :";
             // 
             // btnAddProducts
             // 
             this.btnAddProducts.Font = new System.Drawing.Font("Bahnschrift Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddProducts.Location = new System.Drawing.Point(356, 298);
+            this.btnAddProducts.Location = new System.Drawing.Point(343, 257);
             this.btnAddProducts.Margin = new System.Windows.Forms.Padding(2);
             this.btnAddProducts.Name = "btnAddProducts";
             this.btnAddProducts.Size = new System.Drawing.Size(86, 28);
@@ -167,7 +167,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(465, 350);
+            this.ClientSize = new System.Drawing.Size(453, 307);
             this.Controls.Add(this.btnAddProducts);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button3);
--- Design370/Supplier_Orders.cs
+++ Design370/Supplier_Orders.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Design370
 {
@@ -26,14 +27,85 @@ namespace Design370
 
         private void Supplier_Orders_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                DBConnection dBConnection = DBConnection.Instance();
+                if (dBConnection.IsConnect())
+                {
+                    string query = "SELECT supplier_order_line.supplier_order_id, supplier_order_line.product_id, product.product_name, supplier_order_line.supplier_order_line_quantity" +
+                        " FROM supplier_order_line INNER JOIN product ON supplier_order_line.product_id=product.product_id";
+                    var command = new MySqlCommand(query, dBConnection.Connection);
+                    var reader = command.ExecuteReader();
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(reader);
+                    reader.Close();
+                    foreach(DataRow item in dataTable.Rows)
+                    {
+                        int i = dgvSupplierOrders.Rows.Add();
+                        dgvSupplierOrders.Rows[i].Cells[0].Value = false;
+                        dgvSupplierOrders.Rows[i].Cells[1].Value = dataTable.Rows[i].ItemArray[0];
+                        dgvSupplierOrders.Rows[i].Cells[2].Value = dataTable.Rows[i].ItemArray[2];
+                        dgvSupplierOrders.Rows[i].Cells[3].Value = dataTable.Rows[i].ItemArray[3];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void BtnReceiveSupplierOrder_Click(object sender, EventArgs e)
         {
-            foreach (DataGridViewRow row in dgvSupplierOrders.SelectedRows)
+            
+            foreach(DataGridViewRow item in dgvSupplierOrders.Rows)
             {
-                // dgvSupplierOrders.Rows.RemoveAt(row.Index);
+                if ((bool)item.Cells[0].Value == true)
+                {
+                    int n = dgvReceivedOrders.Rows.Add();
+                    dgvReceivedOrders.Rows[n].Cells[0].Value = item.Cells[1].Value.ToString();
+                    dgvReceivedOrders.Rows[n].Cells[1].Value = item.Cells[2].Value.ToString();
+                    dgvReceivedOrders.Rows[n].Cells[2].Value = item.Cells[3].Value.ToString();
+
+                    
+                }
+
+            }
+
+             dgvSupplierOrders.Rows.Cast<DataGridViewRow>().Where(row => (bool?)row.Cells[0].Value == true)
+             .ToList().ForEach(row => 
+             {
+                 dgvSupplierOrders.Rows.Remove(row);
+             });
+
+            //DBConnection dBConnection = DBConnection.Instance();
+            //try
+            //{
+            //    if (dBConnection.IsConnect())
+            //    {
+            //        string query = "UPDATE `customer` SET `customer_id` = '" + GetCustomerRow + "', `customer_first` = '" + txtCustomerFirst.Text + "', `customer_last` = '" + txtCustomerLast.Text +
+            //            "', `customer_email` = '" + txtCustomerEmail.Text.ToLower() + "', `customer_phone` = '" + txtCustomerPhone.Text + "', `customer_id_number` = '" + txtCustomerID.Text + "' WHERE " +
+            //            "customer_id = '" + GetCustomerRow + "'";
+            //        var command = new MySqlCommand(query, dBConnection.Connection);
+            //        command.ExecuteNonQuery();
+            //    }
+            //}
+            //catch (Exception ee)
+            //{
+            //    MessageBox.Show(ee.Message);
+            //}
+        }
+
+        private void DgvSupplierOrders_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            if((bool)dgvSupplierOrders.SelectedRows[0].Cells[0].Value == false)
+            {
+                dgvSupplierOrders.SelectedRows[0].Cells[0].Value = true;
+            }
+            else
+            {
+                dgvSupplierOrders.SelectedRows[0].Cells[0].Value = false;
             }
         }
     }
